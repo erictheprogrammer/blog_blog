9714c2767d994a4d69a691b6aeecfa709970b0b7
Eric Lima <ericjlima@gmail.com>
2014-03-21 14:14:09 -0400

    commit has a small error where comment doesn't show up.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BlogBlog::Application.config.secret_key_base = '57eed2bcb3ca092ca49ac55f6fcfaa07113fcb878df522b1387818cfbc62a0a36f7954dd0aee963d2e77851509624c1b406f723bddc564f76895a6f9f48acbf9'
